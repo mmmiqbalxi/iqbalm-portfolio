9d2a003ae9c7b64e2c45520b0e01cc13bfb36dca
mmmiqbalxi <mmmiqalxi@gmail.com>
2024-11-30 15:48:28 +0700

    four commit

--- script.js
+++ script.js
@@ -57,3 +57,48 @@ function closeModal() {
   modal.style.display = "none";
 }
 
+// email
+(function(){
+  emailjs.init("qQO5aqRKZflxrO9AY");
+})();
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  // ambil data dari form
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // kirim email dengan EmailJS
+  emailjs.send("service_f65v81e", "template_roxfzse", {
+      from_name: name,
+      from_email: email,
+      message: message
+  })
+  .then(function(response) {
+      console.log('SUCCESS', response.status, response.text);
+
+      // notif
+      Swal.fire({
+          icon: 'success',
+          title: 'Success!',
+          text: 'Message sent successfully!',
+          showConfirmButton: false,
+          timer: 1500
+      });
+
+      // kosongkan form
+      document.getElementById('contact-form').reset();
+  }, function(error) {
+      console.log('FAILED...', error);
+
+      // alert gagal
+      Swal.fire({
+          icon: 'error',
+          title: 'Oops...',
+          text: 'Message failed to send. Please try again',
+          showConfirmButton: true
+      });
+  });
+});

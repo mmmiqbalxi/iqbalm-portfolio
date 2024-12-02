f8d2bc9c526d0cc1f07940119cb29c4bfc8aca1f
mmmiqbalxi <mmmiqalxi@gmail.com>
2024-12-02 23:42:11 +0700

    seven commit

--- script.js
+++ script.js
@@ -15,30 +15,31 @@ themeToggle.addEventListener('click', () => {
 const darkStyles = document.createElement('style');
 darkStyles.textContent = `
   body.dark-theme {
-    background-color: #333;
+    background-color: #000;
     color: #fff;
   }
   body.dark-theme .about {
-    background-color: #444;
+    background-color: #333;
+    color: #fff;
   }
   body.dark-theme .skills h3 {
     color: #fff}
   body.dark-theme .skill-card {
-    background-color: #444}
+    background-color: #333}
   body.dark-theme .skill-card p {
    color: #fff}
   body.dark-theme .project-card {
-   background-color: #444}
+   background-color: #333}
   body.dark-theme .project-card p {
    color: #fff}
   body.dark-theme .contact {
-  background-color: #333}
+  background-color: #000}
   body.dark-theme .contact h3 {
   color: #fff}
-  body.dark-theme .contact .form-group label {
+  body.dark-theme #contact-form label {
   color: #fff}
   body.dark-theme .footer {
-   background-color: #000}
+   background-color: #333}
 `;
 document.head.appendChild(darkStyles);
 
